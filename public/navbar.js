c8bd7890a169ebcd74879eade8130c88321c06e2
vkrpk <victor.krupka@orange.fr>
2021-08-10 08:32:18 +0200

    navbar optimization

--- public/navbar.js
+++ public/navbar.js
@@ -2,6 +2,8 @@ const toggleButton = document.getElementById("toggle-button");
 const sideBar = document.getElementById("side-bar");
 const header = document.getElementById("header");
 const searchInput = document.getElementById("search-input");
+const closeButton = document.getElementById("close-button");
+const nav = document.getElementById("nav");
 
 toggleButton.addEventListener("click", show);
 function changePlaceholder() {
@@ -11,9 +13,14 @@ function changePlaceholder() {
         searchInput.placeholder = "Recherchez un produit..";
     }
 }
+function positionTheCross() {
+    positionCloseButton = closeButton.getBoundingClientRect().right;
+    toggleButton.style.right = positionCloseButton;
+}
 
 function show() {
     header.classList.toggle("active");
+    nav.classList.toggle("active");
     changePlaceholder();
 }
 
@@ -25,8 +32,10 @@ closeOnClick = [content, navbarLg];
 closeOnClick.forEach((element) => {
     element.addEventListener("click", () => {
         header.classList.remove("active");
+        nav.classList.remove("active");
     });
 });
 window.onresize = () => {
     changePlaceholder();
+    // positionTheCross();
 };
