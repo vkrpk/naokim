f18f21b2737c22901a5c99b1f6d6b9a99657488e
victork <victor.krupka@orange.fr>
2022-04-16 16:29:36 +0200

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
    */
   .addEntry('app', './assets/app.js')
   .addEntry('footer', './assets/styles/stylesheets/footer.scss')
+  .addEntry('home', './assets/styles/stylesheets/home.scss')
 
   // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
   .enableStimulusBridge('./assets/controllers.json')
