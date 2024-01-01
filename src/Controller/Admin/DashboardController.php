a0b33e2ed67756a9f75f980d51095c740649820c
vkrpk <victor.krupka@orange.fr>
2024-01-01 09:17:35 +0100

    :rocket: Configuration de l'environnement de développement avec Docker et push de l'image sur github

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -28,7 +28,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new ()
-            ->setTitle('Shopix');
+            ->setTitle('Naokim');
     }
 
     public function configureMenuItems(): iterable
